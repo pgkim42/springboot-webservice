7aed7815660368a0f66f23a60e7dcf037d1f26dc
pgkim42 <vakon1@daum.net>
2022-10-26 21:01:21 +0900

    등록/수정/조회 API 테스트

---
+++ src/main/java/com/pgkim/book/springboot/service/PostsService.java
@@ -0,0 +1,19 @@
+package com.pgkim.book.springboot.service;
+
+import com.pgkim.book.springboot.domain.posts.PostsRepository;
+import com.pgkim.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
